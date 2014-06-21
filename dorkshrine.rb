50612dbc7b1d2641686d78a5011a67c70c8dce45
David Joerg <dsjoerg@gmail.com>
2014-06-21 15:36:40 -0400

    Add milestones for 22 and 44 probes. New argument for how many of your recent matches you want to analyze.

--- dorkshrine.rb
+++ dorkshrine.rb
@@ -44,10 +44,28 @@ def abf_to_events(abf)
   events.sort{|a,b| a[0] <=> b[0]}
 end
 
+def describe_enemy(entity)
+  if (entity['identity']['bnet_id'].to_i == 0)
+    controller = entity['identity']['name']
+  else
+    controller = "Human"
+  end
+  "#{controller} #{race_name(entity['race'])}"
+end
+
+# from ./dorkshrine.rb -m 5116849
 MILESTONES = [
+  [ "22 probes                   ", 4, 17,
+    Proc.new{|army|
+      army["probe"] >= 22.0
+    }],
   [ "6 sentries, zealot, stalker ", 7, 53,
     Proc.new{|army|
       army["stalker"] >= 1.0 && army["zealot"] >= 1.0 && army["sentry"] >= 6.0
+    }],
+  [ "44 probes                   ", 7, 56,
+    Proc.new{|army|
+      army["probe"] >= 44.0
     }],
   [ "66 probes                   ", 10, 55,
     Proc.new{|army|
@@ -82,111 +100,122 @@ FRAMES_PER_SECOND = 16
 GOAL_SLIPPAGE_SECONDS_PERMITTED = 30
 BASE_BUILD_FRAMES = 100 * FRAMES_PER_SECOND
 
+def analyze_match(the_match)
+
+  entities = the_match['entities']
+  our_entity = entities.select{|entity| entity['race'] == 'P'}[0]
+  if our_entity.nil?
+    puts "Hmm, doesn't look like there was a Protoss player in match ##{the_match['id']}"
+    exit
+  end
+  $player_id = our_entity['identity']['id']
+  enemy_entity = entities.reject{|entity| entity['race'] == 'P'}[0]
+
+  # get base start times
+  http_result = Curl::Easy.perform("https://gg2-matchblobs-prod.s3.amazonaws.com/#{the_match['id']}")
+  json_result = http_result.body_str
+  matchblob = JSON.parse(json_result)
+  our_base_lives = matchblob['num_bases'].select{|part| part[0] == $player_id}[0][1]
+  ets = expansion_times(our_base_lives)
+  ets = ets.map{|et| [0, et - BASE_BUILD_FRAMES].max}
+
+  # get scouting time
+  scouting_info = matchblob['scouting']
+  if scouting_info.nil?
+    puts "Please re-upload this replay to GGTracker get information about scouting."
+    first_scout_command_frame = 0
+  else
+    first_scout_command_frame = matchblob['scouting'][$player_id.to_s]
+  end
+
+  # get time at which we have 6 sentries, zealot, and a stalker
+  the_abf = matchblob['armies_by_frame'][$player_id.to_s]
+  the_events = abf_to_events(the_abf)
+  milestone_frames = Array.new()
+  the_army = Hash.new(0.0)
+
+  # walk through them, keeping track of army
+  the_events.each{ |event|
+    the_army[event[1]] += event[2]
+    MILESTONES.each_with_index{ |milestone, i|
+      if milestone_frames[i].nil? && milestone[3].call(the_army)
+        milestone_frames[i] = event[0]
+      end
+    }
+  }
+
+  benchmarks = [
+                ["First scout command", first_scout_command_frame,   1, 13],
+                ["Second base started", ets[0],        3, 41],
+                ["Third base started", ets[1],        8, 8],
+               ]
+
+  MILESTONES.each_with_index {|milestone, i|
+    benchmarks <<
+    [
+     milestone[0],
+     milestone_frames[i],
+     milestone[1], milestone[2]
+    ]
+  }
+
+  # show benchmarks in the order that the player achieved them
+  benchmarks.sort! { |a,b|
+    a_time = a[1]
+    b_time = b[1]
+    if a_time.nil? || a_time == 0
+      a_time = 1000000
+    end
+    if b_time.nil? || b_time == 0
+      b_time = 1000000
+    end
+    a_time <=> b_time
+  }
+
+  puts "%-30s %s" % ["Map", the_match['map_name']]
+  puts "%-30s %s" % ["Enemy", "#{describe_enemy(enemy_entity)}"]
+  benchmarks.each {|benchmark|
+    goal_seconds = benchmark[2] * 60 + benchmark[3]
+    goal_frames = goal_seconds * FRAMES_PER_SECOND
+    if benchmark[1].nil? || benchmark[1] == 0 || benchmark[1] > goal_frames + (GOAL_SLIPPAGE_SECONDS_PERMITTED * FRAMES_PER_SECOND)
+      goal_grade = "WORK ON THIS"
+    elsif benchmark[1] >= goal_frames
+      goal_grade = "   GOOD!    "
+    else
+      goal_grade = "  GREAT!    "
+    end
+    puts "%-30s %5s %12s (goal: %5s)" % [benchmark[0], frames_to_display(benchmark[1]), goal_grade, frames_to_display(goal_frames)]
+  }
+
+  puts "%-30s %s" % ["Game over", seconds_to_display(the_match['duration_seconds'])]
+  puts "%-30s %s" % ["Result", our_entity['win'] ? 'VICTORY' : 'DEFEAT']
+  puts "%-30s %s" % ["Match", "http://ggtracker.com/matches/#{the_match['id']}"]
+end
+
+
+
+$num_to_show = 1
+
 OptionParser.new do |o|
   o.on('-m GGTRACKER_MATCH_ID') { |match_id| $match_id = match_id.to_i }
   o.on('-p GGTRACKER_PLAYER_ID') { |player_id| $player_id = player_id.to_i }
+  o.on('-n num_to_show') { |num_to_show| $num_to_show = num_to_show.to_i }
   o.on('-h') { puts o; exit }
   o.parse!
 end
 if $player_id.nil? == $match_id.nil?
-  $stderr.puts "Usage: dorkshrine.rb [-p <player_id> | -m <match_id>]"
+  $stderr.puts "Usage: dorkshrine.rb [-p <player_id> [-n num_to_show] | -m <match_id>]"
   exit
 end
 
 if $match_id.nil?
   # get latest PvZ for the indicated player
-  matches = json_from_url("http://api.ggtracker.com/api/v1/matches?game_type=1v1&identity_id=#{$player_id}&page=1&paginate=true&race=protoss&game_type=1v1")
-  the_match = matches["collection"][0]
+  matches = json_from_url("http://api.ggtracker.com/api/v1/matches?game_type=1v1&identity_id=#{$player_id}&page=1&paginate=true&race=protoss&game_type=1v1&limit=#{$num_to_show}")
+  matches["collection"].each {|match|
+    analyze_match(match)
+    puts ""
+  }
 else
   the_match = json_from_url("http://api.ggtracker.com/api/v1/matches/#{$match_id}.json")
+  analyze_match(the_match)
 end
-
-# show some basic match details
-entities = the_match['entities']
-our_entity = entities.select{|entity| entity['race'] == 'P'}[0]
-if our_entity.nil?
-  puts "Hmm, doesn't look like there was a Protoss player in match ##{the_match['id']}"
-  exit
-end
-$player_id = our_entity['identity']['id']
-enemy_entity = entities.reject{|entity| entity['race'] == 'P'}[0]
-
-# get base start times
-http_result = Curl::Easy.perform("https://gg2-matchblobs-prod.s3.amazonaws.com/#{the_match['id']}")
-json_result = http_result.body_str
-matchblob = JSON.parse(json_result)
-our_base_lives = matchblob['num_bases'].select{|part| part[0] == $player_id}[0][1]
-ets = expansion_times(our_base_lives)
-ets = ets.map{|et| [0, et - BASE_BUILD_FRAMES].max}
-
-# get scouting time
-scouting_info = matchblob['scouting']
-if scouting_info.nil?
-  puts "Please re-upload this replay to GGTracker get information about scouting."
-  first_scout_command_frame = 0
-else
-  first_scout_command_frame = matchblob['scouting'][$player_id.to_s]
-end
-
-# get time at which we have 6 sentries, zealot, and a stalker
-the_abf = matchblob['armies_by_frame'][$player_id.to_s]
-the_events = abf_to_events(the_abf)
-milestone_frames = Array.new()
-the_army = Hash.new(0.0)
-
-# walk through them, keeping track of army
-the_events.each{ |event|
-  the_army[event[1]] += event[2]
-  MILESTONES.each_with_index{ |milestone, i|
-    if milestone_frames[i].nil? && milestone[3].call(the_army)
-      milestone_frames[i] = event[0]
-    end
-  }
-}
-
-benchmarks = [
-["First scout command", first_scout_command_frame,   1, 13],
-["Second base started", ets[0],        3, 41],
-["Third base started", ets[1],        8, 8],
-]
-
-MILESTONES.each_with_index {|milestone, i|
-  benchmarks <<
-  [
-   milestone[0],
-   milestone_frames[i],
-   milestone[1], milestone[2]
-  ]
-}
-
-# show benchmarks in the order that the player achieved them
-benchmarks.sort! { |a,b|
-  a_time = a[1]
-  b_time = b[1]
-  if a_time.nil? || a_time == 0
-    a_time = 1000000
-  end
-  if b_time.nil? || b_time == 0
-    b_time = 1000000
-  end
-  a_time <=> b_time
-}
-
-puts "%-30s %s" % ["Map", the_match['map_name']]
-puts "%-30s %s" % ["Enemy", "#{race_name(enemy_entity['race'])}"]
-benchmarks.each {|benchmark|
-  goal_seconds = benchmark[2] * 60 + benchmark[3]
-  goal_frames = goal_seconds * FRAMES_PER_SECOND
-  if benchmark[1].nil? || benchmark[1] == 0 || benchmark[1] > goal_frames + (GOAL_SLIPPAGE_SECONDS_PERMITTED * FRAMES_PER_SECOND)
-    goal_grade = "WORK ON THIS"
-  elsif benchmark[1] >= goal_frames
-    goal_grade = "   GOOD!    "
-  else
-    goal_grade = "  GREAT!    "
-  end
-  puts "%-30s %5s %12s (goal: %5s)" % [benchmark[0], frames_to_display(benchmark[1]), goal_grade, frames_to_display(goal_frames)]
-}
-
-puts "%-30s %s" % ["Game over", seconds_to_display(the_match['duration_seconds'])]
-puts "%-30s %s" % ["Result", our_entity['win'] ? 'VICTORY' : 'DEFEAT']
-puts "%-30s %s" % ["Match", "http://ggtracker.com/matches/#{the_match['id']}"]
